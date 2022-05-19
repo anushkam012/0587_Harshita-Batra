7a27d4264ea381d01a824475982e1ac8504a4700
Harshita-Batra-1 <harshita0587.be21@chitkara.edu.in>
2022-05-19 19:20:52 +0530

    Intial

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream> //preprocessor - include files
+
+using namespace std; //link object code to other code of std lib
+
+int main()
+{
+//size of int data types
+    cout <<"Size of int: "<< sizeof(int) <<endl; //using inbuild funct sizeof()
+    cout <<"Size of long int: "<< sizeof(long) <<endl;
+    cout <<"Size of short int"<< sizeof(short) <<endl;
+    cout <<"Size of unsigned int"<< sizeof(unsigned) <<endl;
+    cout <<"Size of signed int"<< sizeof(signed) <<endl;
+
+//giving value
+    cout <<"Size of directly int"<< sizeof(89) <<endl;
+
+    int a = 80;
+    cout <<"Size of after defining int"<< sizeof(a) <<endl;
+
+    float b = 6.8f; //write f at end known as reference variaable
+    cout<<sizeof(b)<<endl;
+
+    cout <<"Size of float"<< sizeof(6.8f) <<endl;
+
+    long double d=567.8l;// write l at end or it assumes to be a float no matter of declaring
+    cout<<sizeof(d)<<endl;
+
+    cout <<"Size of long double"<< sizeof(567.81) <<endl; //gives different answers
+    cout <<"Size of long double"<< sizeof(567.8) <<endl;
+
+//size of float data type
+    cout <<"Size of float: "<< sizeof(float) <<endl;
+    cout <<"Size of double"<< sizeof(double) <<endl;
+    cout <<"Size of long double: "<< sizeof(long double) <<endl;
+//size of char data type
+    cout <<"Size of char"<< sizeof(char) <<endl;
+    cout <<"Size of unsigned char"<< sizeof(unsigned char) <<endl;//if  i write only unsiged it will show 4 for int by default
+    return 0;
+}
