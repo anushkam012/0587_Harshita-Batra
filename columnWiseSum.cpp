d05fe7117a067a031ba72a735b4103670cebe362
anushkam012 <101620866+anushkam012@users.noreply.github.com>
2022-05-19 21:15:24 +0530

    Column Wise Sum

---
+++ columnWiseSum.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+#include <cctype> 
+using namespace std; 
+ 
+ 
+int main(){ 
+    int m,n; 
+    cin>>m>>n; 
+    int arr[m][n]; 
+    for(int i=0;i<m;i++){ 
+        for (int j=0;j<n;j++){ 
+            cin>>arr[i][j]; 
+        } 
+    } 
+     
+    for(int j=0;j<n;j++){ 
+        int sum=0; 
+        for(int i=0;i<m;i++){ 
+            sum=sum+arr[i][j]; 
+        } 
+        cout<<sum<<" "; 
+    } 
+}
